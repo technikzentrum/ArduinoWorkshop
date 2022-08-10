f548fa14b19acc4b2bc0c85eb8c2e148a350decb
Marco Gabrecht <m.gabrecht@gmx.de>
2022-08-10 12:02:32 +0200

    RAMP

--- Tag 1/08-RAMP/08-RAMP.ino
+++ Tag 1/08-RAMP/08-RAMP.ino
@@ -1,4 +1,5 @@
-Installiere die Bibiothek Ramp von Sylvain Garnavault. (Sketch -> Bibiotheken einbinden -> Bibiotheken Verwalten ...)
+/*Installiere die Bibiothek Ramp von Sylvain Garnavault. (Sketch -> Bibiotheken einbinden -> Bibiotheken Verwalten ...)
+Dann giebst du RAMP ein, dass 4te Ergebniss sollte Ramp sein, dort auf Installieren klicken.
 Bitte passe das Beispiel(Datei -> Beispiele -> Ramp -> simple_test) simple_test so an, dass die LED innerhalb von einer Sekunde auf 100% geht und danach wieder auf 0
 Spiele gerne mit den verschiednen Rampen Rum
 
@@ -8,3 +9,4 @@ Links:
  *  https://github.com/siteswapjuggler/RAMP
  *  https://www.arduino.cc/reference/de/language/functions/analog-io/analogwrite/
  *  https://randomnerdtutorials.com/esp32-pwm-arduino-ide/
+ */
