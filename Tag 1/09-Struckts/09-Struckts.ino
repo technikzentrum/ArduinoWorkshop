b05cc54bfb8929cc799c10bdf482f0f5c268a5ba
Philip1008 <110967755+Philip1008@users.noreply.github.com>
2022-08-10 13:28:27 +0200

    Update 09-Struckts.ino

--- Tag 1/09-Struckts/09-Struckts.ino
+++ Tag 1/09-Struckts/09-Struckts.ino
@@ -10,16 +10,16 @@
  * https://www.arduino.cc/reference/de/language/functions/communication/serial/parseint/
  */
 
- struct student{    // Erstelle eine Sammlung von Varablen udn gebe ihnen einen neu erstellten Datentypen
-   String name;     // Der Datentype student bekommt das Atribut Name vom Type String
-   int alter;       // Und alter vom Type int
+ struct student{    // Erstelle eine Sammlung von Variablen und gebe ihnen einen neu erstellten Datentypen
+   String name;     // Der Datentyp student bekommt das Atribut Name vom Typ String
+   int alter;       // Und alter vom Typ int
 };
 
 void setup() {
    Serial.begin(115200);
    Serial.println();
-   student A = {"Max Müller", 25};  // Erstele einen student, schnelle schriebweise
-   Serial.println(A.name);          // komme an die Varriable name von student A
+   student A = {"Max Müller", 25};  // Erstelle einen student, schnelle schreibweise
+   Serial.println(A.name);          // komme an die Variable name von student A
    Serial.println(A.alter);
 }
 
