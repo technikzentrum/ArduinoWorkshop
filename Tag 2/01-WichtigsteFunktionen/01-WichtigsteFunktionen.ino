ce4ec0a40736aec9aca25cec05aafaba96f10dd1
Philip1008 <110967755+Philip1008@users.noreply.github.com>
2022-08-11 11:06:28 +0200

    Update 01-WichtigsteFunktionen.ino

--- Tag 2/01-WichtigsteFunktionen/01-WichtigsteFunktionen.ino
+++ Tag 2/01-WichtigsteFunktionen/01-WichtigsteFunktionen.ino
@@ -6,19 +6,19 @@
 #define buttonPin 2     // Defines sind Werte, die im Quellcode genau so gesetzt werden, keine Varriablen
 const int ledPin =  13; // Constanten sind Varriablen, die nicht mehr geändert werden
 
-int buttonState = 0;    // normale Varriablen können verwendet werden und verändert werden.
-                        // Varriablen die außerhalb von Funktionen gesetzt werden können innerhalb von allen benutzt werden.
+int buttonState = 0;    // normale Variablen können verwendet werden und verändert werden.
+                        // Variablen die außerhalb von Funktionen gesetzt werden können innerhalb von allen benutzt werden.
 
 void setup() {
-  pinMode(ledPin, OUTPUT);// Pins müssen als ausgabe oder eingabe definiert werden.
-  pinMode(buttonPin, INPUT);// sie können INPUT, OUTPUT oder INPUT_PULLUP (INPUT_PULLDOWN -> nur ESP32 nur besondere Pins)
+  pinMode(ledPin, OUTPUT);// Pins müssen als Ausgabe oder Eingabe definiert werden.
+  pinMode(buttonPin, INPUT);// Sie können INPUT, OUTPUT oder INPUT_PULLUP (INPUT_PULLDOWN -> nur ESP32 nur besondere Pins)sein.
                          // Input ist hochohmig, Output ist GND(LOW) oder 40ma 3.3V(HIGH), Pullup istmit 10K bis 100K wiederstand realisiert 
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);       // Über RX und TX (An USB angeschlossen) können Daten gesendet werden.
                         // 9600 ist normal für 8Bit Arduinos, 115200 für ESP32
 //ESP32
   ledcSetup(0, 5000, 8);                    // Sage der Hardware, dass der LED Channel 0
-                                            // mit einer Ferequenz von 5000Hz und einer 8 bit (0-255) auflösung funktionieren soll
+                                            // mit einer Frequenz von 5000Hz und einer 8 bit (0-255) Auflösung funktionieren soll.
   ledcAttachPin(LED_BUILTIN, 0);            // Sage der Hardware, das Pin 25 (LED) mit Channel 1 gesteuert werden soll.
 
 }
@@ -31,9 +31,9 @@ void loop() {
   if (buttonState == HIGH) {
     // turn LED on:
     digitalWrite(ledPin, HIGH);
-    int fadeValue = random(100, 256); // Varriablen innerhalb einer Funktion und innerhalb eines Blocks {..} sind nur dadrin aufrufbar und werden danach verworfen.
+    int fadeValue = random(100, 256); // Variablen innerhalb einer Funktion und innerhalb eines Blocks {..} sind nur da drin aufrufbar und werden danach verworfen.
     analogWrite(LED_BUILTIN, fadeValue);
-    ledcWrite(0, fadeValue);                          // Setze den LEd Channel 0 auf die aktuelle Zahl
+    ledcWrite(0, fadeValue);                          // Setze den LED Channel 0 auf die aktuelle Zahl
   } else {
     // turn LED off:
     digitalWrite(ledPin, LOW);
