febd577b52bd9873077d465bd1e12a0d401fc24c
Marco Gabrecht <m.gabrecht@gmx.de>
2022-08-12 11:48:27 +0200

    Structs fix

--- Tag 1/09-Struckts/09-Struckts.ino
+++ Tag 1/09-Struckts/09-Struckts.ino
@@ -1,42 +1,61 @@
 /*
- * Structs sind sammlungen von Variablen.
- * Es kann als Objekte ohne Funktionen gesehen werden.
- * Aufgabe: Erweitere das Beispiel um Spitzname und Telefonnummer
- * 
- * Beispiel von: https://www.tutorialspoint.com/structs-in-arduino-program#
- * 
- * 
- * https://www.arduino.cc/reference/de/language/functions/communication/serial/readstring/
- * https://www.arduino.cc/reference/de/language/functions/communication/serial/parseint/
- */
+   Structs sind sammlungen von Variablen.
+   Es kann als Objekte ohne Funktionen gesehen werden.
+   Aufgabe: Erweitere das Beispiel um Spitzname und Telefonnummer
 
- struct student{    // Erstelle eine Sammlung von Variablen und gebe ihnen einen neu erstellten Datentypen
-   String name;     // Der Datentyp student bekommt das Atribut Name vom Typ String
-   int alter;       // Und alter vom Typ int
+   Beispiel von: https://www.tutorialspoint.com/structs-in-arduino-program#
+
+
+   https://www.arduino.cc/reference/de/language/functions/communication/serial/readstring/
+   https://www.arduino.cc/reference/de/language/functions/communication/serial/parseint/
+*/
+
+ struct student{    // Erstelle eine Sammlung von Varablen udn gebe ihnen einen neu erstellten Datentypen
+   String name;     // Der Datentype student bekommt das Atribut Name vom Type String
+   int alter;       // Und alter vom Type int
 };
 
 void setup() {
-   Serial.begin(9600);
-   Serial.println();
-   student A = {"Max Müller", 25};  // Erstelle einen student, schnelle schreibweise
-   Serial.println(A.name);          // komme an die Variable name von student A
-   Serial.println(A.alter);
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // warte auf die Serielleverbindung.
+  }
+  delay(1000);// warte auf die Serielleverbindung.
+  Serial.println();
+  student A = {"Max Müller", 25, "Maxi", "+491523498674"};  // Erstele einen student, schnelle schriebweise
+  Serial.println(A.name);          // komme an die Variable name von student A
+  Serial.println(A.alter);
+  Serial.println("");
 }
 
 void loop() {
-   student B = {};
-   Serial.print("Name des Studenten: ");
-   B.name = Serial.readString();
-   Serial.print("Wie alt ist ");
-   Serial.print(B.name);
-   Serial.print(": ");
-   B.alter = Serial.parseInt();
-   Serial.print(B.name);
-   Serial.print(" (");
-   Serial.print(B.alter);
-   Serial.println(" Jahre)");
+  student B = {};            // Erstelle einen Studenten
+  
+  //Name
+  Serial.print("Name des Studenten: ");// Frage nach dem namen
+  while (Serial.available() == 0) { // Warte auf eine eingabe
+    delay(100);
+  }
+  B.name = Serial.readString();    // Lese die eingabe ein
+  B.name = B.name.substring(0, B.name.length() - 1); // Scheinde den Zeilenumbruch weg.
+  Serial.println(B.name);// Schreibe den Namen in die Console
+  
+  //Alter
+  Serial.print("Wie alt ist ");// Frage nach dem Alter
+  Serial.print(B.name);        // Schreibe den Namen
+  Serial.print(": ");
+  while (Serial.available() == 0) { // Warte auf die Eingabe
+    delay(100);
+  }
+  B.alter = Serial.parseInt(); // Lese die eingabe als Zahl ein
+  Serial.readString();         // Clear everything else
+  Serial.println(B.alter);
+  
+  Serial.println("");
+  Serial.print("Name: " );     // Gebe den Studenten aus
+  Serial.println(B.name);
+  Serial.print("Alter: ");
+  Serial.print(B.alter);
+  Serial.println(" Jahre");
+  Serial.println("");
 }
-
-// Gibt als Lösung nur folgendes aus:
-// Spitzname des Studenten: Wie lautet die Telefonnummer von : Name des Studenten: Wie alt ist :  (0 Jahre)
-// Auch die Lösung funktioniert nicht. Man kann keine Eingaben tätigen. Das Beispiel muss insgesamt überarbeitet werden.
